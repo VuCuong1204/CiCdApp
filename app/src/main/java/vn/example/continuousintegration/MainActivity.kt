48fc95824b5a86e2c885b4b7e13e16af8deb0254
cuongvq <cuongvq@ftech.ai>
2024-06-17 22:01:19 +0700

    ci v5

--- app/src/main/java/vn/example/continuousintegration/MainActivity.kt
+++ app/src/main/java/vn/example/continuousintegration/MainActivity.kt
@@ -1,10 +1,9 @@
 package vn.example.continuousintegration
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
-    var a = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
